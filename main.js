17ff90aef85b885349a6e538008af8544213892f
Chance Lane <lanecw@gmail.com>
2020-12-12 11:15:40 -0500

    resturcture loops practice

---
+++ main.js
@@ -0,0 +1,22 @@
+// loops
+
+let moviesArray = ['it', 'et', 'up', 'it 2', 'd2', 'us', 'red', 'go']
+let i = 0;
+
+while(i < 100){
+    console.log('Tweet Tweet')
+    i++
+}
+
+let favoriteFoods = ['pizza', 'steak', 'fried chicken']
+
+for (let i = 0; i < favoriteFoods.length; i++){
+    console.log(favoriteFoods[i])
+}
+
+
+var cohortOneStudents = ['Sable', 'David', 'Joey', 'Nick', 'Tommy', 'Connor', 'Charles', 'Russ', 'Bobby', 'Josh', 'Nikki', 'Matt', 'Dale', 'Sydney']
+for (let i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i])
+}
+
